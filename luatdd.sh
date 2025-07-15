6db7b51fa3c062f961f2ce8dfbfe408e2a9a3668
David Bowling <mailbowling@gmail.com>
2025-07-15 03:06:16 -0500

    added version number
    * Added version number 0.1.0.
    * Improved formatting of test report output.
    * Removed some dead code.

--- luatdd.sh
+++ luatdd.sh
@@ -3,9 +3,9 @@
 # A script for monitoring Lua files under TDD.
 #set -e
 
+luatdd_version=0.1.0
 file_pattern="*.lua"
-driver="ex05-07_test.lua"
-msg="CTRL-C to quit"
+msg="luatdd version $luatdd_version: CTRL-C to quit"
 
 match_test='_test.lua$'
 all_tests=$(find -type f | grep $match_test | sort)
@@ -18,7 +18,7 @@ do
     do
         lua "$test"
     done
-    echo "$msg"
+    printf "%s\n\n" "$msg"
     watch -d -t -n 1 -p -g "ls -lR $file_pattern" > /dev/null
     test_count=$((test_count + 1))
 done
